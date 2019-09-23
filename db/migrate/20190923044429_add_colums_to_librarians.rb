00c0ef4900e77bc4ca2f1e45b0928b93df8e8988
Sanket <sanpai23@bitbucket.org>
2019-09-23 01:17:16 -0400

    Update Librarian Migration

--- db/migrate/20190923044429_add_colums_to_librarians.rb
+++ db/migrate/20190923044429_add_colums_to_librarians.rb
@@ -1,7 +1,6 @@
 class AddColumsToLibrarians < ActiveRecord::Migration[5.2]
   def change
     add_column :librarians, :email, :string, unique: true
-    add_column :librarians, :name, :string
     add_column :librarians, :password, :string
     add_column :librarians, :libraryId, :integer
   end
