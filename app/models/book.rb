386137d3eb53abe6a7bf3eba6af8cd2f6b804a1a
Ridhim Rastogi <rrastog3@ncsu.edu>
2019-09-23 00:45:03 -0400

    Add Books, Library attributes

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,15 @@
 class Book < ApplicationRecord
+    belongs_to :library
+
+    validates :title , presence: true
+    validates :isbn , presence: true
+    validates :author , presence: true
+    validates :language , presence: true
+    validates :published , presence: true
+    validates :edition , presence: true
+    validates :cover_image , presence: true
+    validates :subject , presence: true
+    validates :summary , presence: true
+    validates :special_collection , presence: true
+
 end
--- app/models/library.rb
+++ app/models/library.rb
@@ -1,2 +1,9 @@
 class Library < ApplicationRecord
+    has_many :books
+    
+    validates :name , presence: true
+    validates :university , presence: true
+    validates :location , presence: true
+    validates :max_days_borrowed , presence: true
+    validates :overdue_fines , presence: true
 end
