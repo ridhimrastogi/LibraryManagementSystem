e62c67ef927193184c0a17c93e3463b986c70233
Ridhim Rastogi <rrastog3@ncsu.edu>
2019-09-28 02:44:31 -0400

    Update Library MVC

--- app/controllers/libraries_controller.rb
+++ app/controllers/libraries_controller.rb
@@ -10,6 +10,7 @@ class LibrariesController < ApplicationController
   # GET /libraries/1
   # GET /libraries/1.json
   def show
+    @university = University.find_by('id', :university_id)
   end
 
   # GET /libraries/new
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_26_185441) do
+ActiveRecord::Schema.define(version: 2019_09_28_023651) do
 
   create_table "admins", force: :cascade do |t|
     t.string "name"
@@ -22,7 +22,6 @@ ActiveRecord::Schema.define(version: 2019_09_26_185441) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
@@ -54,7 +53,6 @@ ActiveRecord::Schema.define(version: 2019_09_26_185441) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer "role"
     t.index ["email"], name: "index_librarians_on_email", unique: true
     t.index ["reset_password_token"], name: "index_librarians_on_reset_password_token", unique: true
   end
@@ -85,25 +83,6 @@ ActiveRecord::Schema.define(version: 2019_09_26_185441) do
     t.datetime "remember_created_at"
     t.index ["email"], name: "index_students_on_email", unique: true
     t.index ["reset_password_token"], name: "index_students_on_reset_password_token", unique: true
-  end
-
-  create_table "universities", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "role"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   create_table "universities", force: :cascade do |t|
